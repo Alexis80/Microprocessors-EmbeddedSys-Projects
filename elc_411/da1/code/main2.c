4496ea09e5735e0112ae2b149c4a207c8235a335
Alexis80 <31485756+Alexis80@users.noreply.github.com>
2017-09-05 15:20:18 -0400

    9/5

---
+++ elc_411/da1/code/main2.c
@@ -0,0 +1,68 @@
+/* ========================================
+Lexi Adie and Madie Mastroberte
+ELC 411
+Dr. Pearlstein
+Design Assignment 1
+ * ========================================
+*/
+#include <project.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <ctype.h> 
+#include <stdint.h>
+
+//macro which holds the register address
+#define NVIC_CPUID_BASE_ADDR (0xe000ed00)
+
+int main(void) 
+{   
+    
+    //declares the 32 bit unsigned integers
+    uint32_t implementer;
+    uint32_t variant;
+    uint32_t partno;
+    uint32_t constant;
+    uint32_t revision;
+    uint32_t reg_val;
+    
+    //declares a pointer to a volatile unsigned integer
+    uint32_t volatile *my_reg_ptr;
+    
+    //make the pointer point to NVIC_CPUID_BASE_ADDR register
+    my_reg_ptr = ( uint32_t *) NVIC_CPUID_BASE_ADDR;
+    
+    //puts the value of the pointer into reg val
+    reg_val = *my_reg_ptr;
+    
+    // Access and Extract Register 
+        
+            //take the values of the register and stores them into
+            //corresponding varaibles.
+            implementer = (reg_val >> 24) & 0xFF;
+            variant     = (reg_val >> 20) & 0xF;
+            constant    = (reg_val >> 16) & 0xF;
+            partno      = (reg_val >> 04) & 0xFFF;
+            revision    = (reg_val >> 00) & 0xF;
+                
+     // Manipulate Char String
+        
+            //declares char string for 4.2
+            char myname[] = "Alexis Kathleen Adie";
+            int n,j,k;
+            char temp;
+           
+            //bubble sort
+            for(j=1; j<n; j++)
+            {
+                for(k=0; k<n-j; k++)
+                {
+                    if((int)myname[k]>=(int)myname[k+1])
+                    {
+                        temp=myname[k];
+                        myname[k]=myname[k+1];
+                        myname[k+1]=temp;
+                    }
+                }
+            }      
+        printf( "The string is: <<%s>>\n", myname);
+}
