c64759659ca66e726ecf882596210b753cbd4f45
Alexis80 <31485756+Alexis80@users.noreply.github.com>
2017-09-19 09:45:23 -0400

    Code for lab 2 with both part 1 and part 2

---
+++ elc_343/lab2/code/main.c
@@ -0,0 +1,31 @@
+
+#include <project.h>
+
+int main()
+{
+    CyGlobalIntEnable; 
+
+    //infinite loop that produces a square wave
+
+    for(;;)
+    {   
+        //PART 2 CODE
+        
+        //Pin_2=button so when the button is pressed
+        //Pin_3 (LED2) goes off since the circuit is pulled 
+        //and the circuit is disconnected
+        Pin_3_Write(Pin_2_Read());
+        
+        //PART 1 CODE 
+        
+        //Sets Pin_1(LED1) to one and then sets it off for //500 ms
+        Pin_1_Write(1);
+        CyDelay(1000);
+        //Sets Pin_1(LED1) to zero and then sets it off for //500 ms
+        Pin_1_Write(0);
+        CyDelay(1000);
+        
+    }
+}
+
+
